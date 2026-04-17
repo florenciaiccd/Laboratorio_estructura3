c846d6585159a1e11f194e481b02aecf4d81e407
Your Name <you@example.com>
2026-04-17 22:13:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ int is_equal(void* key1, void* key2){
 // Inicialice el índice current a -1.
 
 HashMap * createMap(long capacity) {
-    HashMap * Mapa = (HashMap*)calloc(capacity, sizeof(HashMap));
+    HashMap * Mapa = (HashMap*)calloc(1, sizeof(HashMap));
     Mapa -> buckets = (Pair**)calloc(capacity, sizeof(Pair*));
     
     for(int i = 0; i < capacity; i++){
