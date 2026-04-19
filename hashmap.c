f35a2524298772df5fb497d1db177a90611e2d34
Your Name <you@example.com>
2026-04-19 21:47:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -139,7 +139,7 @@ void eraseMap(HashMap * map,  char * key) {
     }
 
     else if(strcmp(map -> buckets[posicion] -> key, key) == 0){
-        map -> buckets[posicion] -> key == NULL;
+        map -> pair -> key == NULL;
         map -> size = map -> size -1;
     }
 
